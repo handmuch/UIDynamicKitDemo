9178e6ca6096f63816f828545c0e74d57aa1dbae
郭建华 <prick.guo@tvbc.com.cn>
2020-09-29 12:04:18 +0800

    Initial Commit

---
+++ UIDynamicKitDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  UIDynamicKitDemo
+//
+//  Created by 郭建华 on 2020/9/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ UIDynamicKitDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  UIDynamicKitDemo
+//
+//  Created by 郭建华 on 2020/9/29.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
